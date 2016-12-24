f0eb6e7326a8cadaa02726641e6f3bd3c3552978
Gonçalo Cabrita <_@gmcabrita.com>
2016-12-24 02:40:03 +0000

    polari

--- antergos-bootstrap.sh
+++ antergos-bootstrap.sh
@@ -5,7 +5,7 @@ set -e
 sudo pacman --noconfirm -Syu
 
 # install a bunch of stuff
-sudo pacman --noconfirm -S ca-certificates zsh shellcheck git clang base-devel automake autoconf readline ncurses openssl libyaml stow libxslt libffi libtool unixodbc m4 glibc gcc-libs zlib wxgtk nvidia nvidia-libgl lib32-nvidia-libgl glu libpng libssh curl wget vlc deluge libdvdcss aspell neovim ranger htop p7zip jq tmux imagemagick ttf-hack ttf-google-fonts jdk8-openjdk jre8-openjdk jre8-openjdk-headless ffmpeg tree scrot arandr dropbox dos2unix numix-icon-theme xclip python2-neovim python-neovim texlive-most pandoc steam obs-studio docker keybase gconf gconf-editor dconf dconf-editor
+sudo pacman --noconfirm -S ca-certificates zsh shellcheck git clang base-devel automake autoconf readline ncurses openssl libyaml stow libxslt libffi libtool unixodbc m4 glibc gcc-libs zlib wxgtk nvidia nvidia-libgl lib32-nvidia-libgl glu libpng libssh curl wget vlc deluge libdvdcss aspell neovim ranger htop p7zip jq tmux imagemagick ttf-hack ttf-google-fonts jdk8-openjdk jre8-openjdk jre8-openjdk-headless ffmpeg tree scrot arandr dropbox dos2unix numix-icon-theme xclip python2-neovim python-neovim texlive-most pandoc steam obs-studio docker keybase gconf gconf-editor dconf dconf-editor polari
 
 # setup docker for non-root
 sudo groupadd docker
