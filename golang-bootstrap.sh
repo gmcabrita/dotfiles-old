c2255d63a3aec5b92d51ab3783082aff91b979a0
Gonçalo Cabrita <_@gmcabrita.com>
2017-03-01 20:37:34 +0000

    add readme and license generation

--- golang-bootstrap.sh
+++ golang-bootstrap.sh
@@ -27,5 +27,7 @@ go get -u github.com/uber-common/cpustat
 go get -u github.com/google/gops
 go get -u github.com/rakyll/hey
 go get -u github.com/gmcabrita/stats
+go get -u github.com/golang/dep/...
+curl https://glide.sh/get | sh
 
 gometalinter --install
