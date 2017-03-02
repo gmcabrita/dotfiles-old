518e894faf0717daf43e3da6aebe613b6385ff6b
Gonçalo Cabrita <_@gmcabrita.com>
2017-03-02 00:21:28 +0000

    add script to randomly generate and set a macaddr

---
+++ .bin/airport.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ue;
+
+if [ -z "${1:-}" ]; then
+	echo "Usage: ./airport.sh <interface>";
+	exit 1;
+fi;
+
+iface=$1;
+hexchars="0123456789ABCDEF";
+end=$( for _ in {1..10} ; do echo -n ${hexchars:$((RANDOM % 16)):1} ; done | sed -e 's/\(..\)/:\1/g' );
+MAC=00$end;
+
+ifconfig "$iface" down;
+ifconfig "$iface" hw ether "$MAC";
+ifconfig "$iface" up;
+
+echo "New MAC address: $MAC";
