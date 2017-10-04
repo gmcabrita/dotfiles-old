7ff21843831f8aad9a6699117bfbfe4f15c4b562
Gonçalo Cabrita <_@gmcabrita.com>
2017-10-04 23:15:41 +0100

    force installation of rust crates

--- rustup-bootstrap.sh
+++ rustup-bootstrap.sh
@@ -13,26 +13,26 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 	rustup component add rust-analysis --toolchain nightly
 	rustup component add rust-src --toolchain nightly
 	rustup component add rust-src
-	cargo install racer
-	cargo install rustsym
-	cargo install rustfmt
-	cargo install cargo-edit
-	cargo install cargo-watch
-	cargo install cargo-profiler
-	cargo install cargo-modules
-	cargo install cargo-outdated
-	cargo install cargo-benchcmp
-	cargo install cargo-vendor
-	cargo install cargo-audit
-	cargo install cargo-check
-	cargo install cargo-safety
-	cargo install cargo-update
-	cargo install cargo-release
-	cargo install cargo-count
-	cargo install cargo-tree
-	cargo install cargo-graph
-	cargo install cargo-license
-	cargo install ripgrep
-	rustup run nightly cargo install cargo-fuzz
-	rustup run nightly cargo install clippy
+	cargo install racer --force
+	cargo install rustsym --force
+	cargo install rustfmt --force
+	cargo install cargo-edit --force
+	cargo install cargo-watch --force
+	cargo install cargo-profiler --force
+	cargo install cargo-modules --force
+	cargo install cargo-outdated --force
+	cargo install cargo-benchcmp --force
+	cargo install cargo-vendor --force
+	cargo install cargo-audit --force
+	cargo install cargo-check --force
+	cargo install cargo-safety --force
+	cargo install cargo-update --force
+	cargo install cargo-release --force
+	cargo install cargo-count --force
+	cargo install cargo-tree --force
+	cargo install cargo-graph --force
+	cargo install cargo-license --force
+	cargo install ripgrep --force
+	rustup run nightly cargo install cargo-fuzz --force
+	rustup run nightly cargo install clippy --force
 fi;
