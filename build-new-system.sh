1c677407ed7bdc9680c6305a74d52a8e857664c7
Gonçalo Cabrita <_@gmcabrita.com>
2017-12-07 00:03:47 +0000

    fix file path

--- build-new-system.sh
+++ build-new-system.sh
@@ -214,7 +214,7 @@ echo "";
 if [[ $REPLY =~ ^[Yy]$ ]]; then
 	curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 	sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-	sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > etc/apt/sources.list.d/microsoft.list'
+	sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/microsoft.list'
 	sudo apt-get update
 	sudo apt-get -y install procdump
 fi;
