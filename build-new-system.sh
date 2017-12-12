935386a43f8df0dab4b5b7844daa4ed6adca080f
Gonçalo Cabrita <_@gmcabrita.com>
2017-12-12 19:16:10 +0000

    spotify

--- build-new-system.sh
+++ build-new-system.sh
@@ -188,3 +188,14 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 	sudo apt-get update
 	sudo apt-get -y install procdump
 fi;
+
+# Spotify
+read -rp "Do you want to install Spotify? (y/n) " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+	echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+	sudo apt-get update
+	sudo apt-get -y install spotify-client
+fi;
+
