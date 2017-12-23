8f5832f13114c420ac3589d071a0f070a107adb6
Gonçalo Cabrita <_@gmcabrita.com>
2017-12-23 21:51:30 +0000

    fix spotify local play

--- build-new-system.sh
+++ build-new-system.sh
@@ -178,6 +178,13 @@ rm ./*.deb
 
 sudo cp -r .fonts/* /usr/share/fonts
 
+#### Fix so spotify can play local files
+
+sudo wget -N https://github.com/ramedeiros/spotify_libraries/raw/master/libavcodec.so.54.71.100 -O /usr/lib/x86_64-linux-gnu/libavcodec.so.54
+sudo wget -N https://github.com/ramedeiros/spotify_libraries/raw/master/libavformat.so.54.36.100 -O /usr/lib/x86_64-linux-gnu/libavformat.so.54
+sudo wget -N https://github.com/ramedeiros/spotify_libraries/raw/master/libavutil.so.52.6.100 -O /usr/lib/x86_64-linux-gnu/libavutil.so.52
+sudo ldconfig
+
 #### Run remaining bootstraps
 
 ./asdf-bootstrap.sh
