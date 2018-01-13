b5ca6cdd5da16385f85565f8ded28ad85a012999
Gonçalo Cabrita <_@gmcabrita.com>
2018-01-13 16:55:06 +0000

    add vscode

--- install.sh
+++ install.sh
@@ -98,6 +98,10 @@ setup_sources() {
     # sublime-text 3
     curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
     echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list
+
+    # vscode
+    curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | apt-key add -
+    echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
 }
 
 # installs the vanilla gnome desktop and a nice shell and icon theme
@@ -216,6 +220,7 @@ full() {
         zeal \
         enpass \
         sublime-text \
+        code \
         spotify-client \
         google-chrome-stable \
         dropbox \
