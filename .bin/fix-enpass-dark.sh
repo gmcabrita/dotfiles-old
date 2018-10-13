21d3caf7b7cd4e6e9264bb08ed3883758307bbdf
Gonçalo Cabrita <_@gmcabrita.com>
2018-10-13 18:19:47 +0100

    remove fix-enpass-dark.sh

--- .bin/fix-enpass-dark.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-set -o pipefail
-
-# forces enpass to use a light theme (their dark theme is fucked)
-sudo sed -i '$iexport QT_STYLE_OVERRIDE=fusion' /opt/Enpass/bin/runenpass.sh
