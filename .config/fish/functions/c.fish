11c24632a65d5d100e72138c40df91d3ff8be873
Gonçalo Cabrita <_@gmcabrita.com>
2019-04-16 19:57:19 +0100

    Edge case for `c` when there are no arguments

--- .config/fish/functions/c.fish
+++ .config/fish/functions/c.fish
@@ -1,7 +1,11 @@
 function c
-  if test -f $argv
-    cd (dirname $argv)
+  if test -n "$argv"
+    if test -f $argv
+      cd (dirname $argv)
+    else
+      cd $argv
+    end
   else
-    cd $argv
+    cd
   end
 end
