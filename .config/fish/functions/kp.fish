e95afa373e8841be0cab847b4f16e12d78dff499
Gonçalo Cabrita <_@gmcabrita.com>
2019-04-23 13:10:15 +0100

    fzf & rg

---
+++ .config/fish/functions/kp.fish
@@ -0,0 +1,18 @@
+function kp --description "Kill processes"
+  set -l __kp__pid ''
+
+  if contains -- '--tcp' $argv
+    set __kp__pid (lsof -Pwni tcp | sed 1d | eval "fzf $FZF_DEFAULT_OPTS -m --header='[kill:tcp]'" | awk '{print $2}')
+  else
+    set __kp__pid (ps -ef | sed 1d | eval "fzf $FZF_DEFAULT_OPTS -m --header='[kill:process]'" | awk '{print $2}')
+  end
+
+  if test "x$__kp__pid" != "x"
+    if test "x$argv[1]" != "x"
+      echo $__kp__pid | xargs kill $argv[1]
+    else
+      echo $__kp__pid | xargs kill
+    end
+    kp
+  end
+end
