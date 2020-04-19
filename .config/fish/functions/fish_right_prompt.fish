fee5a1413904d27e71dad8c64d2a1ff16cc6f64a
Gonçalo Cabrita <_@gmcabrita.com>
2020-04-19 12:24:05 +0100

    Add a fish_right_prompt with the duration of the last command if it took more than 1.5 seconds

---
+++ .config/fish/functions/fish_right_prompt.fish
@@ -0,0 +1,8 @@
+function fish_right_prompt
+  if [ $CMD_DURATION -gt 1500 ]
+    set duration = (math $CMD_DURATION/1000)
+    set time_substr (date -d@$duration -u +%T)
+    set miliseconds_substr (math (date -d@$duration -u +%-N)/1000000)
+    printf "%s.%s" (echo $time_substr | sed -E "s/^(00:)+//") $miliseconds_substr
+  end
+end
