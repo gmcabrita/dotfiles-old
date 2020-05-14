7f30dad52fc9c214fa54d824975c97590e3e0fd6
Gonçalo Cabrita <_@gmcabrita.com>
2020-05-14 21:21:28 +0100

    update

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -3,6 +3,7 @@ function fish_right_prompt
     set duration = (math $CMD_DURATION/1000)
     set time_substr (date -d@$duration -u +%T)
     set miliseconds_substr (math (date -d@$duration -u +%-N)/1000000)
-    printf "%s.%s" (echo $time_substr | sed -E "s/^(00:)+//") $miliseconds_substr
+    set pretty_duration (printf "%s.%s" (echo $time_substr | sed -E "s/^(00:)+//") $miliseconds_substr)
+    printf "%s" $pretty_duration
   end
 end
